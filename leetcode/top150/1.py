9cf68a79236eb4ead1f4c1d4a75baa8298081242
Joseph Allan <joseph.s.allan@gmail.com>
2023-08-16 19:55:31 -0300

    Redoing problem 1 with O(n)

--- leetcode/top150/1.py
+++ leetcode/top150/1.py
@@ -16,12 +16,25 @@ example3_out = [0, 1]
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSumV1(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        temp_hash = {}
+        for i in range(len(nums)):
+            remainder = target - nums[i]
+            if remainder in temp_hash:
+                return [temp_hash[remainder], i]
+            temp_hash[nums[i]] = i
+        return []
+
+
+print(Solution().twoSumV1(example1_arg1, example1_arg2) == example1_out)
+print(Solution().twoSumV1(example2_arg1, example2_arg2) == example2_out)
+print(Solution().twoSumV1(example3_arg1, example3_arg2) == example3_out)
 
 print(Solution().twoSum(example1_arg1, example1_arg2) == example1_out)
 print(Solution().twoSum(example2_arg1, example2_arg2) == example2_out)
